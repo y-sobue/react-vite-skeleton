32028443ed443cc6744d506e856687fd1e0f4827
y-sobue <y.sobue@arkth.co.jp>
2022-01-29 00:22:36 +0900

    remove:sample-build,auto-format

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   root: './src',
   build: {
-    outDir: '../build',
+    outDir: '../build'
   },
-  plugins: [react()],
+  plugins: [react()]
 })
